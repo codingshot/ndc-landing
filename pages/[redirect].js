3eb9af2f35b22fa5fa51ae7d808f5556f7161e1b
codingshot <45281667+codingshot@users.noreply.github.com>
2023-09-16 08:50:41 -0400

    added budget lol, transparency

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -211,6 +211,7 @@ const RedirectLinks = {
 "/greylist-appeal": "https://docs.google.com/forms/d/e/1FAIpQLSfa3S6Qz1MdiJEXINMICv9O9oVD7LOdwiPmHI_4_gCLAhN0CA/viewform",
 "/graylist-appeal": "https://docs.google.com/forms/d/e/1FAIpQLSfa3S6Qz1MdiJEXINMICv9O9oVD7LOdwiPmHI_4_gCLAhN0CA/viewform",
 "/voter-graph": "https://near.social/mob.near/widget/VotersGraph",
+"/cap-tracker": "https://near-ndc.notion.site/30f413a8ed9f4a57975570eeb1bc0596?v=8c9c0e2fa0224ae782e23d1ca0774c3b",
 
 
 };
